40ec91ebc7a1a0a151dca00643ec413042e0b305
Whiskeyi <825906196@qq.com>
2022-04-18 15:24:51 +0800

    4.18 coding

---
+++ 字节笔试股票打新.js
@@ -0,0 +1,29 @@
+let line1 = readline().split(' ').map(Number); // ['170074784','21']
+let saveNum = line1[0]; // new
+let lineNext = readline(); // '4 4796'
+let key = 0;
+let rz = [];
+while(lineNext !== 'END') {
+    let lineNumber = lineNext.split(' '); // ['4','4796']
+    lineNumber.shift();
+    // let numberLen = parseInt(lineNumber.shift()); // 4
+    for(let i = 0; i <= line1[1]; i++) {
+        lineNumber.forEach((item) => {
+            if(line1[0].toString().includes(item)) {
+                key = 1;
+                rz.push(line1[0]);
+            }
+        })
+        line1[0] = parseInt(line1[0]) + 1;
+    }
+    line1[0] = saveNum; // new
+    lineNext = readline();
+}
+if(key === 0) print('NO')
+else {
+    print('YES');
+    while(rz.length) {
+        let s = rz.shift();
+        print(s);
+    }
+}
