4e9f8eeecf941304877c5fde5a2ad425c319dc75
Whiskeyi <825906196@qq.com>
2022-05-12 18:14:44 +0800

    5.12 coding

--- test.js
+++ test.js
@@ -735,3 +735,44 @@ console.log(x++ + ++x + x) // 1 + 3 + 3 */
 /* var str = "what is this";
 var k = str.search("is");
 console.log(k) */
+
+/* new Promise((resolve, reject) => {
+  console.log("外部promise");
+  resolve();
+})
+  .then(() => {
+    console.log("外部第一个then");
+    new Promise((resolve, reject) => {
+      console.log("内部promise");
+      resolve();
+    })
+      .then(() => {
+        console.log("内部第一个then");
+      })
+      .then(() => {
+        console.log("内部第二个then");
+      });
+    return new Promise((resolve, reject) => {
+      console.log("内部promise2");
+      resolve();
+    })
+      .then(() => {
+        console.log("内部第一个then2");
+      })
+      .then(() => {
+        console.log("内部第二个then2");
+      });
+  })
+  .then(() => {
+    console.log("外部第二个then");
+  }); */
+
+// 外部promise
+// 外部第一个then
+// 内部promise
+// 内部promise2
+// 内部第一个then
+// 内部第一个then2
+// 内部第二个then
+// 内部第二个then2
+// 外部第二个then
